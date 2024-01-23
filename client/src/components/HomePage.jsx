99a1ee55fa9efaa18896f500a4a3e553b78bf49f
markasilum <62866585+markasilum@users.noreply.github.com>
2024-01-23 13:11:53 +0800

    get person data from database

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const HomePage = () => {
+const [data, setData] = useState([]);
+
+useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/data');
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className='w-9/12 bg-slate-300 h-full p-8'>HomePage</div>
+    
+    <div className='w-9/12 bg-slate-300 h-full p-8'>
+        <h1>Your Data</h1>
+        <ul>
+            {data.map(item => (
+                <li key={item.id}>{item.firstName}{item.middleName}{item.lastName}</li>
+            ))}
+        </ul>
+    </div>
   )
 }
 
