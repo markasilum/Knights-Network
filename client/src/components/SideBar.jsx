957b38da17c758fc74184a4a022f9ccd5e4ad640
markasilum <62866585+markasilum@users.noreply.github.com>
2024-01-29 08:56:49 +0800

    homepage ui update

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -14,7 +14,7 @@ const SideBar = () => {
   return (
     // <div className='w-3/12 bg-slate-500 flex flex-col gap-4 h-screen p-5'>
     
-        <ul className="menu menu-lg bg-base-100 w-3/12 h-screen p-5 gap-3 shadow-lg">
+        <ul className="menu menu-lg bg-base-100 w-3/12 h-screen p-5 gap-3">
           <SidebarButton text={"Profile"}/>
           <SidebarButton text={"Job Posts"}/>
           <SidebarButton text={"Applications"}/>
