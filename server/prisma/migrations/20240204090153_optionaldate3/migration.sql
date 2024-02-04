e2192efc772159a82ca0848b1250eeffeaba5d19
markasilum <62866585+markasilum@users.noreply.github.com>
2024-02-04 20:30:27 +0800

    get-education

---
+++ server/prisma/migrations/20240204090153_optionaldate3/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `education` ADD COLUMN `endDate` DATE NULL,
+    ADD COLUMN `startDate` DATE NULL;
--- server/server.js
+++ server/server.js
@@ -134,6 +134,72 @@ app.put('/api/updateperson',upload.none(), async (req, res) => {
   
   })
 
+//create education
+app.get('/api/geteduc', async (req, res) => {
+  try{    
+    const data = await prisma.education.findMany({
+      where:{
+        personId:'9689255f-6e15-4073-8c68-5d39ad8f9003',
+      },
+    });
+
+    res.json(data);
+    
+  }catch(error){
+    console.error('Error getting degree:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+    console.log(req.body)
+
+  }
+
+})
+
+//create education
+app.post('/createeducation',upload.none(), async (req, res) => {
+  console.log(req.body)
+  try {
+    // Extract data from the request body
+    const { schoolName, degree, qpi, startDate, endDate, awards} = req.body;
+    console.log("startDate "+startDate)
+    // Create a new person record in the database using Prisma
+    const newEducation = await prisma.education.create({ 
+      data:{
+        schoolName, 
+        qpi, 
+        startDate,
+        endDate,
+        awards,
+        person:{
+          connect:{
+              id:"9689255f-6e15-4073-8c68-5d39ad8f9003",
+          }
+        },
+        degree:{
+          connectOrCreate:{
+            where: {
+              degreeName: degree
+            },
+            create: {
+              degreeName: degree
+            },
+          }
+        }
+      },
+      include: {
+        person: true,
+        degree: true,
+      },
+    });
+
+    // Send a response with the newly created person
+    res.status(201).json(newEducation);
+    console.log(newEducation)
+  } catch (error) {
+    console.error('Error creating person:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+    // console.log(req.body)
+  }
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
