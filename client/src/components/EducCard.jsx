3d635919e10f212acf7e8f7ca70f5e27648efbc7
markasilum <62866585+markasilum@users.noreply.github.com>
2024-02-05 09:50:58 +0800

    made-educcard-component

--- client/src/components/EducCard.jsx
+++ client/src/components/EducCard.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import DateToWords from './DateFormatter';
 
-const EducCard = () => {
+const EducCard = ({educData}) => {
+  const[degree, setDegree] = useState([])
+  const degreeIds = educData.map(educ => educ.degreeId)
+  
+  useEffect(()=>{
+    const fetchDegree = async () =>{
+      try {
+        const response = await fetch(`http://localhost:3000/api/getdegree?ids=${degreeIds.join(',')}`);
+        const getUserResult = await response.json();
+        setDegree(getUserResult);
+        // console.log(getUserResult)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchDegree()
+    
+  },[]);
   return (
-    <div>EducCard</div>
+    <div className=''>
+        <div className='flex flex-row justify-between border-b-2 border-solid border-neutral mb-2'>
+            <span className='font-bold'>Education</span>
+            <Link to="/credentials" className='font-thin underline'>Edit</Link>
+        </div>
+        
+        {educData.map((education)=>(
+          <div key={education.id} className='flex flex-col mb-5'>
+              <span className='font-semibold'>{education.schoolName}</span>
+              {degree.find((deg) => deg.id === education.degreeId) && (
+              <span className=''>
+                {
+                  degree.find((deg) => deg.id === education.degreeId).degreeName
+                }
+              </span>
+            )}
+              <div className='flex flex-row gap-1'>
+              <span className='font-thin'><DateToWords dateString={education.startDate} /></span>
+              <span className='font-thin'>-</span>
+              <span className='font-thin'><DateToWords dateString={education.endDate} /></span>
+              </div>
+              <span className='font-thin'>{"QPI: " +education.qpi}</span>
+              <span className='font-thin'>{education.awards}</span>
+
+
+          </div>
+        ))}
+        
+      
+      </div>
   )
 }
 
--- client/src/components/PersonCredentials.jsx
+++ client/src/components/PersonCredentials.jsx
@@ -1,65 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import {
-  BrowserRouter as Router,
-  Route, useParams,
-  Link
-} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import DateToWords from './DateFormatter';
+import EducCard from './EducCard';
 
 const PersonCredentials = ({educData}) => {
-  const[degree, setDegree] = useState([])
-  const degreeIds = educData.map(educ => educ.degreeId)
   
-  useEffect(()=>{
-    const fetchDegree = async () =>{
-      try {
-        const response = await fetch(`http://localhost:3000/api/getdegree?ids=${degreeIds.join(',')}`);
-        const getUserResult = await response.json();
-        setDegree(getUserResult);
-        // console.log(getUserResult)
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-
-    fetchDegree()
-    
-  },[]);
-
-  console.log(degree)
-
   return (
     <div className='overflow-auto-y w-full  bg-white h-fit mt-3 p-5 flex flex-col rounded-xl mb-5 gap-5'>
-      <div className=''>
-        <div className='flex flex-row justify-between border-b-2 border-solid border-neutral mb-2'>
-            <span className='font-bold'>Education</span>
-            <Link to="/credentials" className='font-thin underline'>Edit</Link>
-        </div>
-        
-        {educData.map((education)=>(
-          <div key={education.id} className='flex flex-col mb-5'>
-              <span className='font-semibold'>{education.schoolName}</span>
-              {degree.find((deg) => deg.id === education.degreeId) && (
-              <span className=''>
-                {
-                  degree.find((deg) => deg.id === education.degreeId).degreeName
-                }
-              </span>
-            )}
-              <div className='flex flex-row gap-1'>
-              <span className='font-thin'><DateToWords dateString={education.startDate} /></span>
-              <span className='font-thin'>-</span>
-              <span className='font-thin'><DateToWords dateString={education.endDate} /></span>
-              </div>
-              <span className='font-thin'>{"QPI: " +education.qpi}</span>
-              <span className='font-thin'>{education.awards}</span>
-
-
-          </div>
-        ))}
-        
-      
-      </div>
+      <EducCard educData={educData}/>
 
       <div className='font-semibold'>Experience
       
