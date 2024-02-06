421233bcf59816070b61669c3bd12191d91e21aa
markasilum <62866585+markasilum@users.noreply.github.com>
2024-02-06 18:15:51 +0800

    added-create-and-update-company

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -2,27 +2,54 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.education.createMany({
-        data: [
-          {
-            personId: 'b38c6bf3-59f0-41d3-824b-01ae40b18d67', // Replace with actual person ID
-            schoolName: 'University of XYZ',
-            awards: 'Dean\'s List',
-            qpi: '3.8',
-            startDate: new Date('2018-09-01'),
-            endDate: new Date('2022-05-15'),
-            degreeId: '0fb6b524-cf41-4b0a-95bd-3e74abef6d5e', // Replace with actual degree ID
-          },
-          {
-            personId: 'e532f787-eb8b-4dfa-8d14-518cb5096c68', // Replace with actual person ID
-            schoolName: 'ABC College',
-            startDate: new Date('2015-08-15'),
-            endDate: new Date('2018-05-30'),
-            degreeId: '0bd97286-8a2b-40a3-9b38-97dc621cbe08', // Replace with actual degree ID
-          },
-          // Add more education records as needed
-        ],
-      });
+ await prisma.user.create({
+    data: {
+      username: "smccorp",
+      password: "password123",
+      streetAddress: "40 San Miguel Avenue",
+      cityName: "Mandaluyong City",
+      countryName: "Philippines",
+      zipCode: "1550",
+      contactNum: "(+632) 8-632-2000",
+      emailAddress: "smccorp@example.com",
+      biography: "San Miguel Corporation (SMC) is one of the Philippines’ largest and most diversified conglomerates, with revenues equivalent to about 4% of the country’s GDP in 2020. Originally founded in 1890 as a single product brewery in the Philippines, SMC today has highly integrated operations with ownership in market leading businesses and investments in various sectors, such as beverages, food, packaging, energy, fuel and oil, infrastructure, property development and leasing, cement, car distributorship and banking services. SMC has a portfolio of companies that is interwoven into the economic fabric of the Philippines, benefiting from, as well as contributing to the development and economic progress of the nation.",
+      verified: true,
+      role: {
+        create: {
+          roleName: "company"
+        }
+      },
+      company: {
+        create: {
+          companyName: "San Miguel Corporation",
+          companySize: "10,000+"
+          // Add more fields if needed
+        }
+      }
+    }
+  })
+
+    // await prisma.education.createMany({
+    //     data: [
+    //       {
+    //         personId: 'b38c6bf3-59f0-41d3-824b-01ae40b18d67', // Replace with actual person ID
+    //         schoolName: 'University of XYZ',
+    //         awards: 'Dean\'s List',
+    //         qpi: '3.8',
+    //         startDate: new Date('2018-09-01'),
+    //         endDate: new Date('2022-05-15'),
+    //         degreeId: '0fb6b524-cf41-4b0a-95bd-3e74abef6d5e', // Replace with actual degree ID
+    //       },
+    //       {
+    //         personId: 'e532f787-eb8b-4dfa-8d14-518cb5096c68', // Replace with actual person ID
+    //         schoolName: 'ABC College',
+    //         startDate: new Date('2015-08-15'),
+    //         endDate: new Date('2018-05-30'),
+    //         degreeId: '0bd97286-8a2b-40a3-9b38-97dc621cbe08', // Replace with actual degree ID
+    //       },
+    //       // Add more education records as needed
+    //     ],
+    //   });
     // const users =  [
     //       {
     //         firstName: 'John',
--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ app.use(cors())
 // app.use(express.json())  
 // app.use(express.urlencoded({extended: false}))
 const upload = multer();
+let personUserId = "2e3d06a3-fcdd-45a8-a4d3-2d6cfaad96be"
+let companyUserId = "9113d0aa-0d6a-4df3-b663-d72f3b9d7774"
 
+let companyId = "7c2b0ac0-a50b-4ff5-9b0c-b7c13d45a4fe"
 app.get('/api/data', async (req, res) => {
     const data = await prisma.person.findUnique({
       where:{
@@ -26,7 +29,7 @@ app.get('/api/data', async (req, res) => {
   app.get('/api/getuser', async (req, res) => {
     const data = await prisma.user.findUnique({
       where:{
-        id: "2e3d06a3-fcdd-45a8-a4d3-2d6cfaad96be",
+        id: companyUserId,
       },}
     );
     
@@ -125,6 +128,98 @@ app.put('/api/updateperson',upload.none(), async (req, res) => {
 
     // Send a response with the newly created person
     res.status(201).json(updatePerson);
+  } catch (error) {
+    console.error('Error creating person:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+    console.log(req.body)
+  }
+
+  
+  })
+//company
+app.get('/api/getcompany', async (req, res) => {
+    const data = await prisma.company.findUnique({
+      where:{
+        id: companyId,
+      },}
+    );
+    
+
+    res.json(data);
+  });
+
+//Create company
+app.post('/api/createcompany',upload.none(), async (req, res) => {
+  const{ companyName,companySize,username,password,streetAddress,cityName,countryName,zipCode,contactNum,emailAddress,biography,} = req.body
+  await prisma.user.create({
+    data: {
+      username,
+      password,
+      streetAddress,
+      cityName,
+      countryName,
+      zipCode,
+      contactNum,
+      emailAddress,
+      biography,
+      verified: false,
+      role: {
+        create: {
+          roleName: "company"
+        }
+      },
+      company: {
+        create: {
+          companyName,
+          companySize,
+          // Add more fields if needed
+        }
+      }
+    }
+  })
+})
+
+app.put('/api/updatecompany',upload.none(), async (req, res) => {
+  //to do: include id in the request body
+  try {
+    // Extract data from the request body
+    const{ companyName,companySize,username,password,streetAddress,cityName,countryName,zipCode,contactNum,emailAddress,biography,} = req.body
+    // Construct the full street address
+
+    const updateUser = await prisma.user.update({
+      where: {
+        id: companyUserId,
+      },
+      data: {
+          username,
+          password,
+          streetAddress,
+          cityName,
+          zipCode,
+          countryName,
+          emailAddress,
+          contactNum,
+          biography,
+          company:{
+              update:{
+                where:{
+                  id: companyId,
+                },
+                data:{
+                  companyName,
+                  companySize,
+                }
+                
+              },
+            },
+        },
+        include: {
+          company: true,
+        },
+    })
+
+    // Send a response with the newly created person
+    res.status(201).json(updateUser);
   } catch (error) {
     console.error('Error creating person:', error);
     res.status(500).json({ error: 'Internal Server Error' });
