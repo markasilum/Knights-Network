4f6345a8c16ec1600d350e3505374b03a29f48ec
markasilum <62866585+markasilum@users.noreply.github.com>
2024-03-13 20:40:01 +0800

    add license

--- client/src/components/LicenseCard.jsx
+++ client/src/components/LicenseCard.jsx
@@ -2,6 +2,23 @@ import React, { useEffect, useState } from 'react'
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import DateToWords from './DateFormatter';
 const LicenseCard = () => {
+  const[licenses, setLicense] = useState([])
+
+useEffect(()=>{
+    const fetchExperience = async () =>{
+      try {
+        const response = await fetch(`http://localhost:3000/license/person/index`);
+        const getUserResult = await response.json();
+        setLicense(getUserResult);
+        // console.log(getUserResult)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchExperience()
+    
+  },[]);
   return (
     <div className=''>
         <div className='flex flex-row justify-between border-b-2 border-solid border-neutral mb-2'>
@@ -11,6 +28,17 @@ const LicenseCard = () => {
             <Link to="/licenseform" className='font-thin underline'>Edit</Link>
             </div>
         </div>
+
+        {/* {licenses.map((license)=>(
+          <div key={license.id} className='flex flex-col mb-5'>
+              
+              <p className='font-thin'><DateToWords dateString={license.licenseValidity} /></p>
+        
+              </div>
+             
+        ))} */}
+        <span className='font-normal'>{licenses}</span>
+        {console.log(licenses)}
       </div>
   )
 }
---
+++ server/routes/licenseRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const multer = require("multer");
+const path = require('path');
+const router = express.Router();
+
+const licenseController = require('../controllers/licenseController')
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null,'./uploads/licensePic');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+var upload = multer({ storage: storage })
+
+router.get('/person/index',licenseController.getPersonLicenses)
+router.post('/create',upload.single('licensePic'), licenseController.createPersonLicense)
+
+
+module.exports = router
