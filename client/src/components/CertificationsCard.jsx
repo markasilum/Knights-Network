e20cfa5d5948b738d3567ff0d4454b8a300a45d3
markasilum <62866585+markasilum@users.noreply.github.com>
2024-03-29 21:58:58 +0800

    add credential modal

--- client/src/components/CertificationsCard.jsx
+++ client/src/components/CertificationsCard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import DateToWords from './DateFormatter';
+import CertificationsForm from './CertificationsForm';
 const CertificationsCard = () => {
   const[certs, setCerts] = useState([])
 
@@ -24,11 +25,12 @@ useEffect(()=>{
         <div className='flex flex-row justify-between border-b-2 border-solid border-neutral mb-2'>
             <span className='font-bold'>Certifications</span>
             <div className='flex gap-3'>
-            <Link to="/certificationform" className='font-thin underline'>Add</Link> 
+            <button className='font-thin underline' onClick={()=>document.getElementById('add_cert').showModal()}>Add</button>
             <Link to="/certificationform" className='font-thin underline'>Edit</Link>
             </div>
            
         </div>
+        <CertificationsForm/>
 
         <span className='font-normal'>{certs.join(', ')}</span>
 
--- client/src/components/SkillsForm.jsx
+++ client/src/components/SkillsForm.jsx
@@ -1,23 +1,20 @@
-import React, { useState } from 'react'
-import TopBar from './topbar';
+import React, { useState } from "react";
+import TopBar from "./topbar";
 
 const SkillsForm = () => {
   const [skillName, setSkillName] = useState("");
- 
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    
+
     const formData = new FormData();
 
     formData.append("skillName", skillName);
     try {
-      const response = await fetch(
-        "http://localhost:3000/skills/create",
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
+      const response = await fetch("http://localhost:3000/skills/create", {
+        method: "POST",
+        body: formData,
+      });
 
       const responseData = await response.json();
 
@@ -29,14 +26,17 @@ const SkillsForm = () => {
     }
   };
 
-
-
-
   return (
-    <div className="w-9/12 bg-neutral  h-screen flex flex-col items-center overflow-auto">
-      <TopBar />
-      <form onSubmit={handleSubmit} className="w-1/2">
-        <div className="flex flex-col bg-base-200 shadow-xl p-10 mt-5 rounded-xl">
+    <dialog id="add_skill" className="modal">
+      
+      <div className="modal-box max-w-2xl bg-base-200">
+      <form method="dialog">
+        {/* if there is a button in form, it will close the modal */}
+        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+          ✕
+        </button>
+      </form>
+        <form onSubmit={handleSubmit}>
           <label className="form-control w-full max-w-xs">
             <div className="label">
               <span className="label-text font-bold">Skill</span>
@@ -44,20 +44,23 @@ const SkillsForm = () => {
           </label>
 
           <input
-              type="text"
-              id="skillname"
-              placeholder="ex. Project Management"
-              className="input input-bordered w-full text-center"
-              value={skillName}
-              onChange={(e) => setSkillName(e.target.value)}
-            />
-          <button type="submit" className={`btn btn-primary w-40 mt-5`}>Add Skill</button>
-
-        </div>
-
+            type="text"
+            id="skillname"
+            placeholder="ex. Project Management"
+            className="input input-bordered w-full text-center"
+            value={skillName}
+            onChange={(e) => setSkillName(e.target.value)}
+          />
+          <button type="submit" className={`btn btn-primary w-40 mt-5`}>
+            Add Skill
+          </button>
+        </form>
+      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button>close</button>
       </form>
-    </div>
-  )
-}
+    </dialog>
+  );
+};
 
-export default SkillsForm
+export default SkillsForm;
