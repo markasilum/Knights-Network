7f6035fae2c5b12dadd7ce4b045c6667e7493715
markasilum <62866585+markasilum@users.noreply.github.com>
2024-03-29 21:03:03 +0800

    add experience modal

--- client/src/components/ExperienceForm.jsx
+++ client/src/components/ExperienceForm.jsx
@@ -15,8 +15,8 @@ const ExperienceForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    console.log(startDate)
-    
+    console.log(startDate);
+
     const formData = new FormData();
 
     formData.append("jobTitle", jobTitle);
@@ -26,13 +26,10 @@ const ExperienceForm = () => {
     formData.append("endDate", endDate);
 
     try {
-      const response = await fetch(
-        "http://localhost:3000/createexperience",
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
+      const response = await fetch("http://localhost:3000/createexperience", {
+        method: "POST",
+        body: formData,
+      });
 
       const responseData = await response.json();
 
@@ -45,9 +42,9 @@ const ExperienceForm = () => {
   };
 
   const handleStartDateChange = (startDate) => {
-    console.log(startDate)
+    console.log(startDate);
     const start = startDate.toISOString();
-    console.log(start)
+    console.log(start);
     setStartDate(start);
   };
 
@@ -57,81 +54,90 @@ const ExperienceForm = () => {
     setEndDate(end);
   };
   return (
-    <div className="w-9/12 bg-neutral  h-screen flex flex-col items-center overflow-auto">
-      <TopBar />
-      <form onSubmit={handleSubmit} className="w-1/2">
-        <div className="flex flex-col bg-base-200 shadow-xl p-10 mt-5 rounded-xl">
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text font-bold">Work Experience</span>
-            </div>
-          </label>
+    <dialog id="add_experience" className="modal">
+      <div className="modal-box max-w-2xl mt-10">
+      <form method="dialog">
+          {/* if there is a button in form, it will close the modal */}
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+        <form onSubmit={handleSubmit}>
+          {/* <div className="flex flex-col bg-base-200 shadow-xl p-10 mt-5 rounded-xl"> */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text font-bold">Work Experience</span>
+              </div>
+            </label>
 
-          <div className="flex flex-col gap-2 w-full">
-          <div className="grid grid-cols-2 gap-3">
-          <input
-              type="text"
-              id="jobtitle"
-              placeholder="Job Title"
-              className="input input-bordered w-full text-center"
-              value={jobTitle}
-              onChange={(e) => setJobTitle(e.target.value)}
-            />
-            <input
-              type="text"
-              id="companyname"
-              placeholder="Company Name"
-              className="input input-bordered w-full text-center"
-              value={companyName}
-              onChange={(e) => setCompanyName(e.target.value)}
-            />
+            <div className="flex flex-col gap-2 w-full">
+              <div className="grid grid-cols-2 gap-3">
+                <input
+                  type="text"
+                  id="jobtitle"
+                  placeholder="Job Title"
+                  className="input input-bordered w-full text-center"
+                  value={jobTitle}
+                  onChange={(e) => setJobTitle(e.target.value)}
+                />
+                <input
+                  type="text"
+                  id="companyname"
+                  placeholder="Company Name"
+                  className="input input-bordered w-full text-center"
+                  value={companyName}
+                  onChange={(e) => setCompanyName(e.target.value)}
+                />
+              </div>
 
-          </div>
-            
-            <textarea
-              type="text"
-              id="jobdetails"
-              placeholder="Job Details"
-              className="textarea textarea-bordered textarea-md w-full"
-              value={jobDetails}
-              onChange={(e) => setJobDetails(e.target.value)}
-            />
-            <div className="grid grid-cols-2 gap-3">
-              <DateTime
-                id="startdate"
-                dateFormat="YYYY-MM"
-                selected={startDate}
-                timeFormat={false}
-                onChange={handleStartDateChange}
-                inputProps={{
-                  placeholder: "Start Date",
-                  className:
-                    "flex flex-col w-full justify-center items-center input input-bordered bg-white text-center",
-                }}
+              <textarea
+                type="text"
+                id="jobdetails"
+                placeholder="Job Details"
+                className="textarea textarea-bordered textarea-md h-52 w-full"
+                value={jobDetails}
+                onChange={(e) => setJobDetails(e.target.value)}
               />
+              <div className="grid grid-cols-2 gap-3">
+                <DateTime
+                  id="startdate"
+                  dateFormat="YYYY-MM"
+                  selected={startDate}
+                  timeFormat={false}
+                  onChange={handleStartDateChange}
+                  inputProps={{
+                    placeholder: "Start Date",
+                    className:
+                      "flex flex-col w-full justify-center items-center input input-bordered bg-white text-center",
+                  }}
+                />
 
-              <DateTime
-                id="enddate"
-                dateFormat="YYYY-MM"
-                selected={endDate}
-                timeFormat={false}
-                onChange={handleEndDateChange}
-                inputProps={{
-                  placeholder: "End Date",
-                  className:
-                    "flex flex-col w-full justify-center items-center input input-bordered bg-white text-center",
-                }}
-              />
+                <DateTime
+                  id="enddate"
+                  dateFormat="YYYY-MM"
+                  selected={endDate}
+                  timeFormat={false}
+                  onChange={handleEndDateChange}
+                  inputProps={{
+                    placeholder: "End Date",
+                    className:
+                      "flex flex-col w-full justify-center items-center input input-bordered bg-white text-center",
+                  }}
+                />
+              </div>
             </div>
-            
-          </div>
-          <button type="submit" className={`btn btn-primary w-40 mt-5`}>Create Experience</button>
-
-        </div>
-
+            <button type="submit" className={`btn btn-primary w-40 mt-5`}>
+              Create Experience
+            </button>
+          {/* </div> */}
+        </form>
+       
+      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button>close</button>
       </form>
-    </div>
-  )
-}
+    </dialog>
+  );
+};
 
-export default ExperienceForm
+export default ExperienceForm;
