a92da2ba471d69980925e890e803471c561323a8
markasilum <62866585+markasilum@users.noreply.github.com>
2024-04-05 12:34:19 +0800

    minor changes

--- client/src/components/SidebarButton.jsx
+++ client/src/components/SidebarButton.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from "react-router-dom";
-import PersonIcon from '@mui/icons-material/Person';
+import {BrowserRouter as Router,Route, Link} from "react-router-dom";
 const SidebarButton = ({onCick, text,icon}) => {
   return (
     // <div className='w-full border-solid	border-2 h-12 flex flex-col justify-center p-5'>{text}</div>
