887ae8dc1d350068f429fa1fc2b6dc2a79b3ed50
markasilum <62866585+markasilum@users.noreply.github.com>
2024-04-22 00:20:00 +0800

    update education

--- server/controllers/educationController.js
+++ server/controllers/educationController.js
@@ -9,6 +9,9 @@ const getEducation = async (req, res) => {
       where: {
         personId: personId,
       },
+      include:{
+        degree:true
+      }
     });
 
     res.json(data);
@@ -62,7 +65,55 @@ const createEducation = async (req, res) => {
   }
 };
 
+const updateEducation = async (req, res) => {
+  try {
+    // Extract data from the request body
+    const { id, schoolName, degree, qpi, startDate, endDate, awards } = req.body;
+    // console.log("startDate " + startDate);
+    // Create a new person record in the database using Prisma
+    const newEducation = await prisma.education.update({
+      where:{
+        id:id
+      },
+      data: {
+        schoolName,
+        qpi,
+        startDate,
+        endDate,
+        awards,
+        person: {
+          connect: {
+            id: personId,
+          },
+        },
+        degree: {
+          connectOrCreate: {
+            where: {
+              degreeName: degree,
+            },
+            create: {
+              degreeName: degree,
+            },
+          },
+        },
+      },
+      include: {
+        person: true,
+        degree: true,
+      },
+    });
+
+    // Send a response with the newly created person
+    res.status(201).json(newEducation);
+  } catch (error) {
+    console.error("Error creating person:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+    // console.log(req.body)
+  }
+};
+
 module.exports = {
   getEducation,
   createEducation,
+  updateEducation,
 };
