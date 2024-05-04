0d38e69d4bc850e7d9dcc4b8953eccf5eb00483b
markasilum <62866585+markasilum@users.noreply.github.com>
2024-05-04 18:01:53 +0800

    resume data conditionally renders

--- client/src/components/ResumeCard.jsx
+++ client/src/components/ResumeCard.jsx
@@ -171,7 +171,7 @@ const ResumeCard = () => {
         <SideBar />
         <div className="flex flex-col w-9/12  h-screen  bg-neutral ">
           <div className="pt-5 pr-5 pl-3 overflow-scroll">
-            <div className="w-full bg-white h-fit min-h-80 p-3 rounded-xl mb-20 flex flex-col">
+            <div className="w-full bg-white h-fit p-3 rounded-xl mb-20 flex flex-col">
               <div className="flex flex-col gap-1 items-center">
                 <h1 className="font-semibold text-lg">
                   {personData.firstName +
@@ -185,106 +185,139 @@ const ResumeCard = () => {
                 <p>{userData.contactNum + " " + userData.emailAddress}</p>
               </div>
 
-              <div className="flex flex-row justify-center border-b-2 border-solid border-black mb-2">
-                <span className="font-bold">Experience</span>
-              </div>
+              {experience.length != 0 && (
+                <div>
+                  <div className="flex flex-row justify-center border-b-2 border-solid border-black mb-2">
+                    <span className="font-bold">Experience</span>
+                  </div>
 
-              <div>
-                {experience.map((experience) => (
-                  <div key={experience.id} className="flex flex-col mb-5">
-                    <span className="font-semibold">{experience.jobTitle}</span>
-                    <span className="">{experience.companyName}</span>
-                    <div className="flex flex-row gap-1">
-                      <span className="font-thin">
-                        <DateToWords dateString={experience.startDate} />
-                      </span>
-                      <span className="font-thin">-</span>
-                      <span className="font-thin">
-                        <DateToWords dateString={experience.endDate} />
-                      </span>
-                    </div>
-                    <ul className="list-disc ml-8 font-thin">
-                      {experience.jobDetails
-                        .split("\r\n")
-                        .map((line, index) => (
-                          <li key={index}>{line}</li>
-                        ))}
+                  <div>
+                    {experience.map((experience) => (
+                      <div key={experience.id} className="flex flex-col mb-5">
+                        <span className="font-semibold">
+                          {experience.jobTitle}
+                        </span>
+                        <span className="">{experience.companyName}</span>
+                        <div className="flex flex-row gap-1">
+                          <span className="font-thin">
+                            <DateToWords dateString={experience.startDate} />
+                          </span>
+                          <span className="font-thin">-</span>
+                          <span className="font-thin">
+                            <DateToWords dateString={experience.endDate} />
+                          </span>
+                        </div>
+                        <ul className="list-disc ml-8 font-thin">
+                          {experience.jobDetails
+                            .split("\r\n")
+                            .map((line, index) => (
+                              <li key={index}>{line}</li>
+                            ))}
+                        </ul>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+
+              {skills.length != 0 && (
+                <div>
+                  <div className="flex flex-row justify-center border-b-2 border-solid border-black mb-2">
+                    <span className="font-bold">Skills</span>
+                  </div>
+
+                  <div className="pl-5">
+                    <ul className="font-normal list-disc">
+                      {skills.map((skill) => (
+                        <li key={skill.id}>{skill.skillName}</li>
+                      ))}
                     </ul>
-                    
                   </div>
-                ))}
-              </div>
+                </div>
+              )}
 
-              <div className="flex flex-row justify-center border-b-2 border-solid border-black mb-2">
-                <span className="font-bold">Skills</span>
-              </div>
-
-              <div className="pl-5">
-                <ul className="font-normal list-disc">
-                  {skills.map((skill) => (
-                    <li key={skill.id}>{skill.skillName}</li>
-                  ))}
-                </ul>
-              </div>
-
-              <div className="flex flex-row justify-center border-b-2 border-solid border-black mb-2">
-                <span className="font-bold">Education</span>
-              </div>
-
-              <div>
-                {educData.map((education) => (
-                  <div key={education.id} className="flex flex-col mb-5">
-                    <span className="font-semibold">
-                      {education.schoolName}
-                    </span>
-                    {degree.find((deg) => deg.id === education.degreeId) && (
-                      <span className="">
-                        {
-                          degree.find((deg) => deg.id === education.degreeId)
-                            .degreeName
-                        }
-                      </span>
-                    )}
-                    <div className="flex flex-row gap-1">
-                      <span className="font-thin">
-                        <DateToWords dateString={education.startDate} />
-                      </span>
-                      <span className="font-thin">-</span>
-                      <span className="font-thin">
-                        <DateToWords dateString={education.endDate} />
-                      </span>
-                    </div>
-                    <span className="font-thin">{"QPI: " + education.qpi}</span>
-                    <span className="font-thin">{education.awards}</span>
+              {educData.length != 0 && (
+                <div>
+                  <div className="flex flex-row justify-center border-b-2 border-solid border-black mb-2">
+                    <span className="font-bold">Education</span>
                   </div>
-                ))}
-              </div>
 
-              <div className="flex flex-row justify-center border-b-2 border-solid border-black mb-2">
-                <span className="font-bold">Licenses</span>
-              </div>
+                  <div>
+                    {educData.map((education) => (
+                      <div key={education.id} className="flex flex-col mb-5">
+                        <span className="font-semibold">
+                          {education.schoolName}
+                        </span>
+                        {degree.find(
+                          (deg) => deg.id === education.degreeId
+                        ) && (
+                          <span className="">
+                            {
+                              degree.find(
+                                (deg) => deg.id === education.degreeId
+                              ).degreeName
+                            }
+                          </span>
+                        )}
+                        <div className="flex flex-row gap-1">
+                          <span className="font-thin">
+                            <DateToWords dateString={education.startDate} />
+                          </span>
+                          <span className="font-thin">-</span>
+                          <span className="font-thin">
+                            <DateToWords dateString={education.endDate} />
+                          </span>
+                        </div>
+                        <span className="font-thin">
+                          {"QPI: " + education.qpi}
+                        </span>
+                        <span className="font-thin">{education.awards}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
 
-              <div className="pl-5">
-                <ul className="font-normal list-disc">
-                  {licenses.map((license) => (
-                    <li key={license.id}>{license.licenseName}</li>
-                  ))}
-                </ul>
-              </div>
+              {licenses.length != 0 && (
+                <div>
+                  <div className="flex flex-row justify-center border-b-2 border-solid border-black mb-2">
+                    <span className="font-bold">Licenses</span>
+                  </div>
 
-              <div className="flex flex-row justify-center border-b-2 border-solid border-black mb-2">
-                <span className="font-bold">Certifications</span>
-              </div>
+                  <div className="pl-5">
+                    <ul className="font-normal list-disc">
+                      {licenses.map((license) => (
+                        <li key={license.id}>{license.licenseName}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              )}
 
-              <div className="pl-5">
-                <ul className="font-normal list-disc">
-                  {certs.map((cert) => (
-                    <li key={cert.id}>{cert.certName}</li>
-                  ))}
-                </ul>
-              </div>
+              {certs.length != 0 && (
+                <div>
+                  <div className="flex flex-row justify-center border-b-2 border-solid border-black mb-2">
+                    <span className="font-bold">Certifications</span>
+                  </div>
+
+                  <div className="pl-5">
+                    <ul className="font-normal list-disc">
+                      {certs.map((cert) => (
+                        <li key={cert.id}>{cert.certName}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              )}
+
               <div className="flex flex-row justify-end w-full">
-                <button type="button" className="btn btn-success w-40 mt-5 mb-3 text-white" onClick={downloadResume}>Downlod PDF</button>
+                <button
+                  type="button"
+                  className="btn btn-success w-40 mt-5 mb-3 text-white"
+                  onClick={downloadResume}
+                >
+                  Downlod PDF
+                </button>
               </div>
             </div>
           </div>
